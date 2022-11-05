acc48e65513332fcbe576eefae2e7c85f671034f
MarkoDunder <marko.dundjer14@gmail.com>
2022-11-05 18:12:35 +0100

    9th commit

--- server/db/index.js
+++ server/db/index.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 
 
 const pool= new Pool({
-    connectionString: process.env.EXTERNAL_URL,
+    
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
-    ssl:true
+    
 });
 
 module.exports={
