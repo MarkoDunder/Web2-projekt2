be70fb345c5605436fae6f32921171231dc10a44
MarkoDunder <marko.dundjer14@gmail.com>
2022-11-05 17:34:10 +0100

    7th commit

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,14 @@
 const {Pool} = require('pg');
 
-const pool= new Pool();
+const pool= new Pool({
+    connectionString: process.env.EXTERNAL_URL,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl:true
+});
 
 module.exports={
     query:(text, params)=> pool.query(text, params),
