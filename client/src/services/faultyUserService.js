0d1c7e491f5eb3b3eec85d2737fa5d313d0c9de3
MarkoDunder <marko.dundjer14@gmail.com>
2022-11-05 15:58:02 +0100

    third commit

--- client/src/services/faultyUserService.js
+++ client/src/services/faultyUserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL=process.env.NODE_ENV === 'production'? "api/v1/user": "http://localhost:3001/api/v1/user"
+const baseURL= "https://sql-injection-server.onrender.com/api/v1/user"
 
 export default axios.create({
     baseURL,
--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL=process.env.NODE_ENV === 'production'? "api/v1/userInfo": "http://localhost:3001/api/v1/userInfo"
+const baseURL= "https://sql-injection-server.onrender.com/api/v1/userInfo"
 
 export default axios.create({
-    baseURL,
+    baseURL
 });
